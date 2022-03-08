b56dbefd4ec1e006f1814c27a10ba7af263a58fa
Test <nabadjebli@gmail.com>
2022-03-08 21:55:55 +0100

    Introduction au JS

---
+++ cv.js
@@ -0,0 +1,21 @@
+let âge = 39;
+console.log(typeof âge);
+console.log(âge);
+
+let nomPrénom = "ADJEBLI Nabil";
+console.log(typeof nomPrénom);
+console.log(nomPrénom);
+
+let élèveWildCodeSchool = false;
+console.log(typeof élèveWildCodeSchool);
+console.log(élèveWildCodeSchool);
+
+let nomEcole = "3i-scools";
+console.log(typeof nomEcole);
+console.log(nomEcole);
+
+âge = 39 + 1;
+console.log(âge);
+
+élèveWildCodeSchool = true;
+console.log(élèveWildCodeSchool);
